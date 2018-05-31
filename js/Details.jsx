f0ba5a32c9edfc071b58617ee12c4557f2f383d0
Sam Fortin <sam.j.fortin@gmail.com>
2018-05-30 20:42:07 -0500

    Spinner component added and details changed to es6 class

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,29 +1,34 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-const Details = (props: { show: Show }) => {
-  const { title, year, description, poster, trailer } = props.show;
-  return (
-    <div className="details">
-      <Header />
-      <section>
-        <h1>{title}</h1>
-        <h2>({year})</h2>
-        <img src={`/public/img/posters/${poster}`} alt={title} />
-        <p>{description}</p>
-      </section>
-      <div>
-        <iframe
-          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameBorder="0"
-          allowFullScreen
-          title={title}
-        />
+class Details extends Component {
+  props: {
+    show: Show
+  }
+  render() {
+    const { title, year, description, poster, trailer } = this.props.show;
+    return (
+      <div className="details">
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          <img src={`/public/img/posters/${poster}`} alt={title} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={title}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Details;
