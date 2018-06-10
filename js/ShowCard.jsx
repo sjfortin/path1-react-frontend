0ab019f57078f0c87ec7d55a11708f5ff9040fb7
Sam Fortin <sam.j.fortin@gmail.com>
2018-06-10 15:12:25 -0500

    Testing the app with React performance tools

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -21,15 +21,34 @@ const ImageWrapper = styled.img`
   width: 50%;
 `;
 
-const ShowCard = (props: Show) => (
-  <Wrapper to={`/details/${props.imdbID}`}>
-    <ImageWrapper src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
-    <div>
-      <h3>{props.title}</h3>
-      <h4>({props.year})</h4>
-      <p>{props.description} <span style={{ fontSize: '12px' }}>{props.imdbID}</span></p>
-    </div>
-  </Wrapper>
-);
+class ShowCard extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  props: Show;
+  render() {
+    return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
+        <ImageWrapper src={`/public/img/posters/${this.props.poster}`} alt={`${this.props.title} Show Poster`} />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description} <span style={{ fontSize: '12px' }}>{this.props.imdbID}</span></p>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+
+// const ShowCard = (props: Show) => (
+//   <Wrapper to={`/details/${props.imdbID}`}>
+//     <ImageWrapper src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
+//     <div>
+//       <h3>{props.title}</h3>
+//       <h4>({props.year})</h4>
+//       <p>{props.description} <span style={{ fontSize: '12px' }}>{props.imdbID}</span></p>
+//     </div>
+//   </Wrapper>
+// );
 
 export default ShowCard;
